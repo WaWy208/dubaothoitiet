bec57b60ce414ce4d9a1c983aa7e33830f001d9b
WaWy208 <0opsnaruto111@gmai.comgit config --global user.email 0opsnaruto111@gmai.com>
2026-04-27 22:18:52 +0700

    thaydoi

---
+++ config.js
@@ -0,0 +1,29 @@
+window.WARDS = [
+  { id: "p_tiengiang", name: "Tiền Giang" },
+  { id: "p_bentre", name: "Bến Tre" },
+  { id: "p_dongthap", name: "Đồng Tháp" },
+  { id: "p_vinhlong", name: "Vĩnh Long" },
+  { id: "p_travinh", name: "Trà Vinh" },
+  { id: "p_haugiang", name: "Hậu Giang" },
+  { id: "p_angiang", name: "An Giang" },
+  { id: "p_soctrang", name: "Sóc Trăng" },
+  { id: "p_kiengiang", name: "Kiên Giang" },
+  { id: "p_baclieu", name: "Bạc Liêu" },
+  { id: "p_camau", name: "Cà Mau" },
+  { id: "p_cantho", name: "Cần Thơ" }
+];
+
+window.WARDS_COORDS = [
+  { id: "p_tiengiang", lat: 10.3542, lon: 106.3574 },
+  { id: "p_bentre", lat: 10.2435, lon: 106.3753 },
+  { id: "p_dongthap", lat: 10.4571, lon: 105.6322 },
+  { id: "p_vinhlong", lat: 10.2524, lon: 105.9754 },
+  { id: "p_travinh", lat: 9.9329, lon: 106.3409 },
+  { id: "p_haugiang", lat: 9.7842, lon: 105.4715 },
+  { id: "p_angiang", lat: 10.3759, lon: 105.4269 },
+  { id: "p_soctrang", lat: 9.6015, lon: 105.9760 },
+  { id: "p_kiengiang", lat: 10.0125, lon: 105.0809 },
+  { id: "p_baclieu", lat: 9.2941, lon: 105.7278 },
+  { id: "p_camau", lat: 9.1769, lon: 105.1505 },
+  { id: "p_cantho", lat: 10.0452, lon: 105.7469 }
+];
